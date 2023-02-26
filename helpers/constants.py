f8ca61482b977e023dd4a75d9b7070b05a418b0d
Santhoshkumard11 <santhoshkdhana@gmail.com>
2023-02-26 16:16:24 +0530

    modify - working models till v3 - add - new models

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,6 +1,6 @@
-model_attributes = {
-    "v1": {
-        "model_name": "v1",
+MODEL_ATTRIBUTES = {
+    "v1.1": {
+        "model_version": "v1.1",
         "features_list": [
             "pH",
             "Iron",
@@ -18,8 +18,8 @@ model_attributes = {
             "Total Dissolved Solids",
         ],
     },
-    "v2": {
-        "model_name": "v2",
+    "v2.0": {
+        "model_version": "v2.0",
         "features_list": [
             "pH",
             "Iron",
@@ -40,7 +40,6 @@ model_attributes = {
             "Air Temperature",
             "Day",
             "Time of Day",
-            "Target",
             "clr_Colorless",
             "clr_Faint Yellow",
             "clr_Light Yellow",
@@ -53,12 +52,79 @@ model_attributes = {
             "src_River",
             "src_Spring",
             "src_Stream",
+            "src_Well",
+        ],
+    },
+    "v3.1": {
+        "model_version": "v3.1",
+        "features_list": [
+            "pH",
+            "Iron",
+            "Nitrate",
+            "Chloride",
+            "Lead",
+            "Zinc",
+            "Turbidity",
+            "Fluoride",
+            "Copper",
+            "Odor",
+            "Sulfate",
+            "Conductivity",
+            "Chlorine",
+            "Manganese",
+            "Total Dissolved Solids",
+            "Water Temperature",
+            "Air Temperature",
+            "Day",
+            "Time of Day",
+            "clr_Colorless",
+            "clr_Faint Yellow",
+            "clr_Light Yellow",
+            "clr_Near Colorless",
+            "clr_Yellow",
+        ],
+    },
+    "v4.1": {
+        "model_version": "v4.1",
+        "features_list": [
+            "pH",
+            "Iron",
+            "Nitrate",
+            "Chloride",
+            "Lead",
+            "Zinc",
+            "Turbidity",
+            "Fluoride",
+            "Copper",
+            "Odor",
+            "Sulfate",
+            "Conductivity",
+            "Chlorine",
+            "Manganese",
+            "Total Dissolved Solids",
+            "Water Temperature",
+            "Air Temperature",
+            "Day",
+            "Time of Day",
+            "clr_Colorless",
+            "clr_Faint Yellow",
+            "clr_Light Yellow",
+            "clr_Near Colorless",
+            "clr_Yellow",
+            "src_Aquifer",
+            "src_Ground",
+            "src_Lake",
+            "src_Reservoir",
+            "src_River",
+            "src_Spring",
+            "src_Stream",
+            "src_Well",
         ],
     },
 }
 
 
-_ML_LOG_QUERY_ARGS_MAPPING = {
+ML_LOG_QUERY_ARGS_MAPPING = {
     "model_version": ["req_body", "model_version"],
     "features_dict": ["req_body", "features_dict"],
     "predicted_class": ["response", "predicted_class"],
@@ -68,10 +134,10 @@ _ML_LOG_QUERY_ARGS_MAPPING = {
     "get_feature_importance": ["req_body", "get_feature_importance"],
     "get_model_features": ["req_body", "get_model_features"],
     "status_code": ["response", "status_code"],
-    "log_source": ["response", "log_source"]
+    "log_source": ["response", "log_source"],
 }
 
-_MISCLASSIFIED_QUERY_ARGS_MAPPING = {
+MISCLASSIFIED_QUERY_ARGS_MAPPING = {
     "model_version": ["req_body", "model_version"],
     "features_dict": ["req_body", "features_dict"],
     "response_time": ["response", "response_time"],
@@ -82,10 +148,18 @@ _MISCLASSIFIED_QUERY_ARGS_MAPPING = {
     "status_code": ["response", "status_code"],
 }
 
-_QUERY_CONSTRUCT_HELPER = {
-    "ml_model_logs": {"table_name": "fresh_water_classifier", "database_name": "ml_model_logs", "column_names": "model_version,features_dict,predicted_class,response_time,_mode,get_probability,get_feature_importance,get_model_features,status_code,log_source"},
-    "misclassified": {"table_name": "misclassified", "database_name": "retrain_model", "column_names": "original_class,predicted_class,features_dict,model_version,response_time"},
+QUERY_CONSTRUCT_HELPER = {
+    "ml_model_logs": {
+        "table_name": "fresh_water_classifier",
+        "database_name": "ml_model_logs",
+        "column_names": "model_version,features_dict,predicted_class,response_time,_mode,get_probability,get_feature_importance,get_model_features,status_code,log_source",
+    },
+    "misclassified": {
+        "table_name": "misclassified",
+        "database_name": "retrain_model",
+        "column_names": "original_class,predicted_class,features_dict,model_version,response_time",
+    },
 }
 
 
-_INSERT_QUERY_TEMPLATE = """INSERT INTO {database_name}.{table_name} ({column_names})  values ({column_values})"""
+INSERT_QUERY_TEMPLATE = """INSERT INTO {database_name}.{table_name} ({column_names})  values ({column_values})"""
--- helpers/utils.py
+++ helpers/utils.py
@@ -1,7 +1,20 @@
-from helpers.constants import model_attributes
+import os
+from helpers.constants import MODEL_ATTRIBUTES
+
+
+_LATEST_MODEL_VERSION = os.environ["LATEST_MODEL_VERSION"]
 
 
 def get_model_attributes(model_version: str) -> dict:
-    model_attributes.update({"latest": model_attributes.get("v2")})
+    """Get the model attributes of the current model version
 
-    return model_attributes.get(model_version)
+    Args:
+        model_version (str): model version from user request body
+
+    Returns:
+        dict: model attributes
+    """
+
+    MODEL_ATTRIBUTES.update({"latest": MODEL_ATTRIBUTES[_LATEST_MODEL_VERSION]})
+
+    return MODEL_ATTRIBUTES[model_version]
